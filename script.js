8ab4972aba0193bd2a59c163948ff7714c88e8c3
hakerdesvertrauens <xcdbxznkm9@privaterelay.appleid.com>
2026-02-01 11:59:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let messageIndex = 0;
-let messages = ["Will you be my Valentine?", "I knew you would say yes ☺️"]
-function handleNoClick() {
-	const noButton = document.querySelector('.no-button');
-	const yesButton = document.querySelector('.yes-button');
-	noButton.textContent = messages [messageIndex];
-	messageIndex = (messageIndex + 1) % messages.length;
-	const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-	yesButton.style.fontSize = `${currentSize * 1.5}px`;
-}
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
 
-function handleYesClick() {
-	window.location.href = 	"yes_page.html";
-}
+let yesSize = 24;
+let noSize = 24;
+
+noBtn.addEventListener("click", () => {
+  yesSize *= 1.25;
+  noSize *= 0.75;
+
+  yesBtn.style.fontSize = yesSize + "px";
+  noBtn.style.fontSize = noSize + "px";
+
+  if (noSize < 8) {
+    noBtn.style.display = "none";
+  }
+});
+
+yesBtn.addEventListener("click", () => {
+  window.location.href = "yes_page.html";
+});
